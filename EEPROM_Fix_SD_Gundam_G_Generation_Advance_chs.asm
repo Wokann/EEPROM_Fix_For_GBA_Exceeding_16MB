7a377d0d8a97dcb4520c574e6738f9d09d5db7dd
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-04-29 00:05:41 +0800

    增加eeprom硬件检查
    但不知为何又丢失了对sram实体卡的兼容性

--- EEPROM_Fix_SD_Gundam_G_Generation_Advance_chs.asm
+++ EEPROM_Fix_SD_Gundam_G_Generation_Advance_chs.asm
@@ -99,23 +99,59 @@ Hack_Address                equ 0x09218380
 .org Hack_Address
 .func Save_Fix
    push r1
-   bl SaveHardware_Check   ;检查硬件,返回r0                       
+   ldr r0,=0x0203FFFC
+   ldr r0,[r0]
+   lsl r0,r0,8
+   lsr r0,r0,8
+   cmp r0,0
+   bne @@RamCanNotBeUsed
+
+ @@RamCanBeUsed:
+   ldr r0,=0x0203FFFC
+   ldrb r0,[r0,3]
+   mov r1,1
+   and r0,r1
+   cmp r0,1
+   beq @@SkipHardware_Check
+ @@DoHardware_Check:
+   bl SaveHardware_Check
+   lsl r0,r0,1
+   add r0,1
+   ldr r1,=0x0203FFFC
+   strb r0,[r1,3]
+ @@SkipHardware_Check:
+   ldr r1,=0x0203FFFC
+   ldrb r0,[r1,3]
+   lsr r0,r0,1
+   b @@CheckResult
+ .pool
+
+ @@RamCanNotBeUsed:
+   bl SaveHardware_Check   ;检查0x0203FFFC-0x0203FFFF是否被使用，若无用则用于存储检查结果，以减少重复计算，
+                           ;若已被使用，则每次均进行硬件检查。
+                           ;检查硬件的结果若存入内存，则检查结果左移1位，将bit0设为1用作已经检查的标志位，再存入内存
+                           ;后续计算需右移1位恢复检查结果的实际值                      
                            ;0b00(0x0):NoSaveHardware 0b01(0x1):HaveEEPROM
                            ;0b10(0x2):HaveSRAM       0b11(0x3):HaveEEPROM_SRAM
-                           ;目前设置除了1固定使用eeprom外，其余都使用sram格式
+                           ;目前设置除了1，3固定使用eeprom外，其余都使用sram格式
+ @@CheckResult:
    pop r1
    lsl r1,r1,0x1F
    lsr r1,r1,0x1F
-   lsl r0,r0,0x1D
-   lsr r0,r0,0x1D
+   lsl r0,r0,0x1E
+   lsr r0,r0,0x1E          ;获取硬件检查结果
    cmp r1,0                ;确认0-读取或1-写入存档
    bne @@SaveWrite
  @@SaveRead:
    cmp r0,1
    beq @@SaveType_EEPROMRead
+   cmp r0,3
+   beq @@SaveType_EEPROMRead
    b @@SaveType_SRAMRead
  @@SaveWrite:
    cmp r0,1
+   beq @@SaveType_EEPROMWrite
+   cmp r0,3
    beq @@SaveType_EEPROMWrite
    b @@SaveType_SRAMWrite
 
@@ -131,58 +167,96 @@ Hack_Address                equ 0x09218380
    ldr r0,=SRAMRead_hack
    mov pc,r0
  .pool
-   ;pop r0-r2
-   ;bl SRAMRead_hack
-   ;b @@End
  @@SaveType_SRAMWrite:
    ldr r0,=SRAMWrite_hack
    mov pc,r0
  .pool
-   ;pop r0-r2
-   ;bl SRAMWrite_hack
-   ;b @@End
 
  @@End:
    pop r1
    bx r1
 .endfunc
+
 .func SaveHardware_Check
-   push r3-r4,lr
-   mov r4,0             ;用于记录存档硬件情况，bit0(1-haveEEPROM),bit0(1-haveSRAM)
+   push r3-r4,lr        ;bit0用作是否检查过的标志位，存放于0x0203FFFF
+   mov r4,0b00         ;用于记录存档硬件情况，bit1(1-haveEEPROM),bit2(1-haveSRAM)
                         ;0b00(0x0):NoSaveHardware 0b01(0x1):HaveEEPROM
                         ;0b10(0x2):HaveSRAM       0b11(0x3):HaveEEPROM_SRAM
  @@SRAM_check:
    mov r1,0xE0
    lsl r1,r1,0x14       ;SRAM存档地址
+ @@BackUpSRAM:
    ldrb r3,[r1]         ;备份该地址上第一个字节
- @@CompareSRAMData:
-   mov r0,0x1
+   mov r0,0x55
+   cmp r0,r3
+   bne @@TestWriteSRAM
+   mov r0,0xAA
+ @@TestWriteSRAM:
    strb r0,[r1]         ;向该地址写入一字节0x1
    ldrb r2,[r1]         ;再向该地址读取一字节，
    cmp r2,r0            ;确认读写是否一致，以判断sram是否正常,对比至少2次，此处为第1次。
    bne @@EEPROM_check   ;sram异常，跳至检查eeprom
-   mov r0,0x2
-   strb r0,[r1]         ;向该地址写入一字节0x2
-   ldrb r2,[r1]         ;再向该地址读取一字节，
-   cmp r2,r0            ;确认读写是否一致，以判断sram是否正常,对比至少2次，此处为第2次。
-   bne @@EEPROM_check   ;sram异常，跳至检查eeprom
  @@HaveSRAM:
    strb r3,[r1]         ;sram正常，将备份的原字节还原
-   add r4,0b10          ;记录标记
+   add r4,0b10          ;记录SRAM标记
    b @@EEPROM_check
 
  @@EEPROM_check:
-   add r4,0b01          ;暂且默认eeprom存在
-   ;暂且不做验证，只要有sram就用sram存档方式
-   ;没有sram，则默认使用eeprom
-   ;若sram和eeprom都不存在，则继续使用eeprom存档
+   sub sp,0x10
+ @@BackUpEEPROM:
+   mov r0,0
+   add r1,sp,8
+   bl EEPROMRead_hack
+   ldr r0,[sp,0x8]                     ;检查备份的8字节是否与校验8字节相同
+   ldr r1,=Strings_For_EEPROM_check
+   ldr r1,[r1]
+   cmp r0,r1
+   bne @@UseStrings1
+   ldr r0,[sp,0xC]
+   ldr r1,=(Strings_For_EEPROM_check+4)
+   ldr r1,[r1]
+   cmp r0,r1
+   beq @@UseStrings2
+ @@UseStrings1:
+   ldr r1,=Strings_For_EEPROM_check
+   b @@TestWriteEEPROM
+ @@UseStrings2:
+   ldr r1,=(Strings_For_EEPROM_check+8)
+
+ @@TestWriteEEPROM:
+   push r1                             ;暂存用于校验的字符地址
+   mov r0,0
+   mov r2,1
+   bl EEPROMWrite_hack                 ;写入校验8字节
+   mov r0,0
+   add r1,sp,4
+   bl EEPROMRead_hack                  ;读取8字节
+   pop r1
+   ldr r1,[r1]
+   ldr r0,[sp]
+   cmp r0,r1                           ;对比字节
+   bne @@End
+
+ @@HaveEEPROM:
+   mov r0,0
+   add r1,sp,8
+   mov r2,1
+   bl EEPROMWrite_hack                 ;还原备份字节
+   add r4,0b01
 
  @@End:
+   add sp,0x10
    mov r0,r4
    pop r3-r4
    pop r1
    bx r1
  .pool
+.endfunc
+
+.align 4
+.func Strings_For_EEPROM_check
+   .byte 0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07
+   .byte 0x0F,0x0E,0x0D,0x0C,0x0B,0x0A,0x09,0x08
 .endfunc
 
 .func DMA3Transfer_copy
@@ -238,70 +312,70 @@ Hack_Address                equ 0x09218380
  .pool
 .endfunc
 
-.func EEPROMRead_hack
+.func EEPROMRead_hack         ;u16 address, u16* data 
     push {r4, r5, r6, lr}
-    sub sp, #0x88
-    add r5, r1, #0
+    sub sp, #0x88             ;u16 buffer[0x44]
+    add r5, r1, #0            ;r5=data
     lsl r0, r0, #0x10
-    lsr r3, r0, #0x10
+    lsr r3, r0, #0x10         ;r3=address
     ldr r0, =gEEPROMConfig
     ldr r0, [r0]
-    ldrh r0, [r0, #4]
+    ldrh r0, [r0, #4]         ;r0=gEEPROMConfig->size
     cmp r3, r0
-    bcc @@InRange
-    ldr r0, =0x80FF
+    bcc @@InRange             ;if (address >= gEEPROMConfig->size)
+    ldr r0, =0x80FF           ;return EEPROM_OUT_OF_RANGE;
     b @@End
  .pool
  @@InRange:
     ldr r1, =gEEPROMConfig
     ldr r0, [r1]
     ldrb r0, [r0, #8]
-    lsl r0, r0, #1
-    mov r4, sp
-    add r2, r0, r4
-    add r2, #2
+    lsl r0, r0, #1            ;r0=(gEEPROMConfig->address_width << 1)
+    mov r4, sp                ;r4=&buffer
+    add r2, r0, r4            ;r2=u16 ptr
+    add r2, #2                ;r2=(gEEPROMConfig->address_width << 1)+&buffer+2
     mov r0, #0
-    strh r0, [r2, #2]
-    strh r0, [r2]
-    mov r4, #0
+    strh r0, [r2, #2]         ;ptr[1]=0
+    strh r0, [r2]             ;ptr[0]=0
+    mov r4, #0                ;r4=t1=0
     ldr r0, [r1]
-    ldrb r0, [r0, #8]
+    ldrb r0, [r0, #8]         ;r0=gEEPROMConfig->address_width
     cmp r4, r0
-    bcs @@Batch1
+    bcs @@Batch1              ;for (t1 = 0; t1 < gEEPROMConfig->address_width; t1++)
     mov r6, #1
  @@Loop1:
-    add r0, r3, #0
+    add r0, r3, #0            ;r0=address
     and r0, r6
-    strh r0, [r2]
-    sub r2, #2
-    lsr r3, r3, #1
-    add r0, r4, #1
+    strh r0, [r2]             ;ptr[0]=1 & address
+    sub r2, #2                ;r2=ptr-2
+    lsr r3, r3, #1            ;r3=address/2
+    add r0, r4, #1            
     lsl r0, r0, #0x18
-    lsr r4, r0, #0x18
+    lsr r4, r0, #0x18         ;r4=t1++
     ldr r0, [r1]
-    ldrb r0, [r0, #8]
-    cmp r4, r0
+    ldrb r0, [r0, #8]         ;r0=gEEPROMConfig->address_width
+    cmp r4, r0                ;for (t1 = 0; t1 < gEEPROMConfig->address_width; t1++)
     bcc @@Loop1
  @@Batch1:
     mov r0, #1
-    strh r0, [r2]
-    sub r2, #2
-    strh r0, [r2]
+    strh r0, [r2]             ;ptr[0]=1
+    sub r2, #2                ;r2=ptr-2
+    strh r0, [r2]             ;ptr[0]=1
     ldr r4, =EEPROM_SaveAddress
     ldr r0, =gEEPROMConfig
     ldr r0, [r0]
     ldrb r2, [r0, #8]
-    add r2, #3
-    mov r0, sp
-    add r1, r4, #0
-    bl DMA3Transfer_copy
-    add r0, r4, #0
-    mov r1, sp
-    mov r2, #0x44
-    bl DMA3Transfer_copy
-    add r2, sp, #8
-    add r5, #6
-    mov r4, #0
+    add r2, #3                ;r2=gEEPROMConfig->address_width + 3
+    mov r0, sp                ;r0=&buffer
+    add r1, r4, #0            ;r1=0x0DFFFF00
+    bl DMA3Transfer_copy      ;DMA3Transfer(buffer, (u16*)0xdffff00, gEEPROMConfig->address_width + 3)
+    add r0, r4, #0            ;r0=0x0DFFFF00
+    mov r1, sp                ;r1=&buffer
+    mov r2, #0x44             ;r2=0x44
+    bl DMA3Transfer_copy      ;DMA3Transfer((u16*)0xdffff00, buffer, 0x44)
+    add r2, sp, #8            ;r2=u16* ptr=&buffer+2*4
+    add r5, #6                ;r5=u16 data+2*3
+    mov r4, #0                ;r4=t1=0
     mov r6, #1
  @@Loop2:
     mov r1, #0
