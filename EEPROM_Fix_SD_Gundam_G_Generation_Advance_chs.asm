a00b197c37b8de6c6eaadd98f5b663c308345a2a
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-04-29 13:10:28 +0800

    增加rom填充说明
    sram卡兼容失败似乎是因为末尾字节未对齐，致使烧录软件对超出16MB部分的rom烧录存在问题。

--- EEPROM_Fix_SD_Gundam_G_Generation_Advance_chs.asm
+++ EEPROM_Fix_SD_Gundam_G_Generation_Advance_chs.asm
@@ -607,10 +607,20 @@ Hack_Address                equ 0x09218380
  .pool
 .endfunc
 
+EndHack:
+   ;满填充模式
+   ;.fill (0x0A000000 - EndHack),0x00
 
-
-
-;EndHack:
-;    .fill (0x0A000000 - EndHack),0x00
+   ;注:若不填充满32MB也可，但需要注意以下使用情况
+   ;   1、SRAM盗卡烧录时，需确保32MB空间全部擦除，避免0x01FFFF00-0x01FFFFFF最后0x100字节有内容残留
+   ;   2、在ezo(de)运行时，由于auto模式读取rom头数据库确定存档类型，无法正常应用修复过的eeprom，解决方法有如下几种：
+   ;     2-1:eeprom修复版rom，将rom头游戏代码改为铸剑3、蜡笔小新等游戏即可(ezo数据库内识别存档模式为0x23)
+   ;     2-2:eeprom修复版rom，进入游戏前设置eeprom8K存档格式打开，且文件大小必须大于0x01200000(小于等于时不会切换为0x23存档模式)
+   ;不填充满32MB模式如下，请注释掉前面的满填充方式
+   .if (EndHack > 0x09200000)
+      .align 16
+   .else
+      .fill (0x09200010 - EndHack),0x00
+   .endif
 
 .close
